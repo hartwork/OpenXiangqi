4cd1eac84c138ab34e3de5909661e9e1e7385be3
Sebastian Pipping <sebastian@pipping.org>
2013-10-03 03:29:18 +0200

    Add basic WXF notation parser

---
+++ src/org/openxiangqi/core/notations/WxfNotationParser.java
@@ -0,0 +1,84 @@
+/*
+ * OpenXiangqi -- Chinese chess implemented in Java
+ * Copyright (C) 2013  Sebastian Pipping <sebastian@pipping.org>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openxiangqi.core.notations;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.openxiangqi.core.exceptions.MalformedNotation;
+import org.openxiangqi.core.geometry.PlayerRelativeLocation.LooseVerticalLocation;
+import org.openxiangqi.core.geometry.PlayerRelativeMove.Direction;
+
+public class WxfNotationParser extends NotationParserBase {
+	// http://www.wxf.org/xq/computer/wxf_notation.html
+	private static String REGEX_PATTERN = "^([KPEARHC])(([1-9])|([+-]))([+.=-])([1-9])$";
+	
+	@Override
+	public Notation parse(String notation, Strictness strictness)
+			throws MalformedNotation {
+		Pattern p = Pattern.compile(REGEX_PATTERN,
+				(strictness == Strictness.STRICT) ? 0
+						: Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(notation);
+
+		if (!m.matches()) {
+			throw new MalformedNotation();
+		}
+
+		String pieceAbbreviationFirst = m.group(1);
+		String horizontalLocationString = m.group(3);
+		String relativeVerticalLocationString = m.group(4);
+		String pieceAbbreviationSecond = pieceAbbreviationFirst;
+		String directionString = m.group(5);
+		String parameterString = m.group(6);
+
+		return assemble(pieceAbbreviationFirst, horizontalLocationString,
+				relativeVerticalLocationString, pieceAbbreviationSecond,
+				directionString, parameterString);
+	}
+
+	protected LooseVerticalLocation lookUpVerticalLocation(
+			char playerRelativeVerticalLocationChar) {
+		LooseVerticalLocation playerRelativeVerticalLocationEnum = null;
+		if (playerRelativeVerticalLocationChar == '+') {
+			playerRelativeVerticalLocationEnum = LooseVerticalLocation.FRONT;
+		} else if (playerRelativeVerticalLocationChar == '-') {
+			playerRelativeVerticalLocationEnum = LooseVerticalLocation.REAR;
+		} else {
+			assert false;
+		}
+		return playerRelativeVerticalLocationEnum;
+	}
+
+	protected Direction lookUpDirection(char directionChar) {
+		Direction directionEnum = null;
+		if (directionChar == '+') {
+			directionEnum = Direction.FORWARD;
+		} else if (directionChar == '-') {
+			directionEnum = Direction.BACKWARD;
+		} else if (directionChar == '.') {
+			directionEnum = Direction.HORIZONTAL;
+		} else if (directionChar == '=') {
+			directionEnum = Direction.HORIZONTAL;
+		} else {
+			assert false;
+		}
+		return directionEnum;
+	}
+
+}
---
+++ src/org/openxiangqi/core/notations/tests/TestWxfNotation.java
@@ -0,0 +1,93 @@
+/*
+ * OpenXiangqi -- Chinese chess implemented in Java
+ * Copyright (C) 2013  Sebastian Pipping <sebastian@pipping.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openxiangqi.core.notations.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openxiangqi.core.exceptions.MalformedNotation;
+import org.openxiangqi.core.geometry.PlayerRelativeLocation.LooseVerticalLocation;
+import org.openxiangqi.core.geometry.PlayerRelativeMove.Direction;
+import org.openxiangqi.core.notations.WxfNotationParser;
+import org.openxiangqi.core.notations.Notation;
+import org.openxiangqi.core.notations.Notation.HorizontalConfiguration;
+import org.openxiangqi.core.notations.NotationParserBase.Strictness;
+
+public class TestWxfNotation {
+
+	@Test
+	public void testStraightValidForward() throws MalformedNotation {
+		Notation receivedNotation = new WxfNotationParser().parse("R1+2",
+				Strictness.STRICT);
+		Notation expectedNotation = new Notation('R',
+				HorizontalConfiguration.DIFFERENT_VERTICAL_LINES, 1, null,
+				Direction.FORWARD, 2);
+		assertEquals(receivedNotation, expectedNotation);
+	}
+
+	@Test
+	public void testStraightValidBackward() throws MalformedNotation {
+		Notation receivedNotation = new WxfNotationParser().parse("R3-4",
+				Strictness.STRICT);
+		Notation expectedNotation = new Notation('R',
+				HorizontalConfiguration.DIFFERENT_VERTICAL_LINES, 3, null,
+				Direction.BACKWARD, 4);
+		assertEquals(receivedNotation, expectedNotation);
+	}
+
+	@Test
+	public void testStraightValidHorizontalDot() throws MalformedNotation {
+		Notation receivedNotation = new WxfNotationParser().parse("R5.6",
+				Strictness.STRICT);
+		Notation expectedNotation = new Notation('R',
+				HorizontalConfiguration.DIFFERENT_VERTICAL_LINES, 5, null,
+				Direction.HORIZONTAL, 6);
+		assertEquals(receivedNotation, expectedNotation);
+	}
+
+	@Test
+	public void testStraightValidHorizontalEqual() throws MalformedNotation {
+		Notation receivedNotation = new WxfNotationParser().parse("R5=6",
+				Strictness.STRICT);
+		Notation expectedNotation = new Notation('R',
+				HorizontalConfiguration.DIFFERENT_VERTICAL_LINES, 5, null,
+				Direction.HORIZONTAL, 6);
+		assertEquals(receivedNotation, expectedNotation);
+	}
+
+	@Test
+	public void testSameVerticalLineFront() throws MalformedNotation {
+		Notation receivedNotation = new WxfNotationParser().parse("R+.7",
+				Strictness.STRICT);
+		Notation expectedNotation = new Notation('R',
+				HorizontalConfiguration.SAME_VERTICAL_LINE, -1,
+				LooseVerticalLocation.FRONT, Direction.HORIZONTAL, 7);
+		assertEquals(receivedNotation, expectedNotation);
+	}
+
+	@Test
+	public void testSameVerticalLineRear() throws MalformedNotation {
+		Notation receivedNotation = new WxfNotationParser().parse("R-.8",
+				Strictness.STRICT);
+		Notation expectedNotation = new Notation('R',
+				HorizontalConfiguration.SAME_VERTICAL_LINE, -1,
+				LooseVerticalLocation.REAR, Direction.HORIZONTAL, 8);
+		assertEquals(receivedNotation, expectedNotation);
+	}
+
+}
